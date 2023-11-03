dd5d1aed36f1569b9f4241d3b063bd02b13bfcc1
Pavel Yakovlev <poulyak@Pavels-MacBook-Pro-2.local>
2023-11-03 15:39:18 +0300

    add visualiser of commits

--- homework5/main.py
+++ homework5/main.py
@@ -13,7 +13,27 @@ def preprocessString(s):
     for i in range(len(s)):
         result.extend(s[i].split())
     # result[5] = datetime.datetime.fromtimestamp(int(result[5])).__str__()
+    result[5] = int(result[5])
     return result
+
+def findCommitMergeWith(branch, t, hist):
+    comm_name = ""
+    curr_time = 0
+    for key in hist:
+        for item in hist[key]:
+            comm_time = item[1]
+            if item[0].startswith(branch) and comm_time<t and comm_time>curr_time:
+                comm_name = item[0]
+                curr_time = comm_time
+
+    return comm_name
+def preprocessMerges(dot, d):
+    for key in d:
+        for item in d[key]:
+            if item[2] == "merge":
+                to_merge = findCommitMergeWith(item[3], item[1], d)
+                dot.append(f"{to_merge} -> {item[0]};")
+
 
 
 def createGraph(graph, hist, path):
@@ -23,24 +43,42 @@ def createGraph(graph, hist, path):
         for line in file.readlines():
             line = preprocessString(line)
 
-            line[0] = line[0][:7]
-            line[1] = line[1][:7]
+            line[0] = current_branch+"_"+line[0][:7]
+            line[1] = current_branch+"_"+line[1][:7]
             print(line)
-            hist[line[0]] = hist.get(line[0], [])+[(line[1], line[2], line[3], line[5], " ".join(line[7:]))]
+            if line[7].startswith("commit"):
+                line[7] = "commit"
+                hist[line[0]] = hist.get(line[0], []) + [(line[1], line[5], line[7])]
+            elif line[7].startswith("merge"):
+                line[7] = "merge"
+                hist[line[0]] = hist.get(line[0], [])+[(line[1], line[5], line[7], line[8].replace(":", ""))]
+            else:
+                hist[line[0]] = hist.get(line[0], []) + [(line[1], line[5], line[7])]
+            # hist[line[0]] = hist.get(line[0], [])+[(line[1], line[2], line[3], line[5], " ".join(line[7:]))]
+
 
 
 
 if __name__ == "__main__":
+
     dot = []
+    dot.append("digraph G {")
+
     tree = {}
     for filename in os.listdir(path_commits):
         f = os.path.join(path_commits, filename)
 
         if os.path.isfile(f):
             createGraph(dot, tree, f)
+    for k, v in tree.items():
+        for child in v:
+            dot.append(f"{k} -> {child[0]};")
+        print(k, v)
+    print("++++++++++++++")
+    preprocessMerges(dot, tree)
 
-    # for k, v in tree.items():
-        # print(k, v)
+    dot.append("}")
+    print("\n".join(dot))
     # dot.render(view=True)
 
 # for i in os.listdir("../.git/objects/61"):
