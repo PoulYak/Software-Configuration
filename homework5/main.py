c423e18def42ef2d998fe76160d18f4cf074ee3e
Pavel Yakovlev <poulyak@Pavels-MacBook-Pro-2.local>
2023-11-03 12:43:20 +0300

    first test commit

--- homework5/main.py
+++ homework5/main.py
@@ -1,26 +1,41 @@
 import os
-import graphviz
 
+import datetime
 
 path_commits = "../.git/logs/refs/heads"
-print(os.listdir())
-
-def createGraph(graph, path):
-    file = open(path, 'r')
-    current_branch = os.path.basename(file.name)
-    print(file.read())
 
 
-dot = graphviz.Digraph('Graph')
-for filename in os.listdir(path_commits):
-    f = os.path.join(path_commits, filename)
+def preprocessString(s):
+    s = s.split("\t")
+    result = []
+    for i in range(len(s)):
+        result.extend(s[i].split())
+    result[5] = datetime.datetime.fromtimestamp(int(result[5])).__str__()
+    return result
 
-    if os.path.isfile(f):
-        f = str(f).replace("\\", '/')
-        createGraph(dot, f)
-        print(f)
+
+def createGraph(graph, hist, path):
+    with open(path, "r") as file:
+        current_branch = os.path.basename(file.name)
+        for line in file.readlines():
+            line = preprocessString(line)
+            if line[7].startswith('commit'):
+                hist[line[0]] = hist.get(line[0], [])+[(line[1], line[2], line[3], line[5])]
+
+
+
+if __name__ == "__main__":
+    dot = []
+    tree = {}
+    for filename in os.listdir(path_commits):
+        f = os.path.join(path_commits, filename)
+
+        if os.path.isfile(f):
+            createGraph(dot, tree, f)
+
+    for k, v in tree.items():
+        print(k, v)
     # dot.render(view=True)
-
 
 # for i in os.listdir("../.git/objects/61"):
 #     with open(os.path.join("../.git/objects/61", i), "rb") as f:
